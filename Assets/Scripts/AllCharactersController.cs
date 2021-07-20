fbfcb329efae92072fe481aaf6c80b8c7ebd8a58
KirillSherbaev2002 <76176852+KirillSherbaev2002@users.noreply.github.com>
2021-07-20 22:16:48 +0300

    Initial commit

---
+++ Assets/Scripts/AllCharactersController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class AllCharactersController : MonoBehaviour
+{
+    private GameObject SplineMoveObject;
+    [SerializeField] private float defaultRotationYOfCharacter;
+
+    private void Awake()
+    {
+        SplineMoveObject = GameObject.FindGameObjectWithTag("SplineMoveObject");
+    }
+    void Update()
+    {
+        transform.position = SplineMoveObject.transform.position;
+        transform.rotation = Quaternion.Euler(SplineMoveObject.transform.rotation.x, SplineMoveObject.transform.rotation.y+ defaultRotationYOfCharacter, SplineMoveObject.transform.rotation.z);
+    }
+}
---
+++ Assets/Scripts/CameraFollows.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraFollows : MonoBehaviour
+{
+    private GameObject Characters;
+    [SerializeField] private Vector3 defaultCamToCharactersPositionDifference;
+
+    private void Awake()
+    {
+        Characters = GameObject.FindGameObjectWithTag("SplineMoveObject");
+        defaultCamToCharactersPositionDifference = transform.position - Characters.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = Characters.transform.position - defaultCamToCharactersPositionDifference;
+    }
+}
